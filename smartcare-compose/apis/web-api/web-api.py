413cd160c5a16096af2909ef08b5f9f8936629ef
brunintrol <b.felipe1234@hotmail.com>
2021-11-01 03:22:45 -0300

    ajustes postgres
    ajustes temporários de senha e arquivos sql

--- smartcare-compose/apis/web-api/web-api.py
+++ smartcare-compose/apis/web-api/web-api.py
@@ -203,10 +203,11 @@ class Env():
             db_host = os.getenv("DB_HOST", "")
             db_user = os.getenv("DB_USER", "")
             db_name = os.getenv("DB_NAME", "")
+            db_pass = os.getenv("DB_PASS", "")
 
-            if db_host != "" and db_user != "" and db_name != "":
+            if db_host != "" and db_user != "" and db_name != "" and db_pass:
 
-                Data.append({"data": f"dbname={db_name} user={db_user} host={db_host}"})
+                Data.append({"data": f"dbname={db_name} user={db_user} password={db_pass} host={db_host}"})
 
             else:
 
--- smartcare-compose/apis/web-api/web-api.sh
+++ smartcare-compose/apis/web-api/web-api.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-
 pip install psycopg2==2.7.3.2 bottle==0.12.13
 python -u web-api.py
