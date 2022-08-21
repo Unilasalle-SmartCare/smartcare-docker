74923c0fe3406faf696d1d3e825dd28346856123
Yuri Ferreira <64503593+Yuri-Ferreira-27@users.noreply.github.com>
2022-08-21 14:44:04 -0300

    Criação das rotas de insert e delete em paciente

--- smartcare-compose/apis/web-api/WebApiPaciente.py
+++ smartcare-compose/apis/web-api/WebApiPaciente.py
@@ -241,4 +241,290 @@ class route:
             Errors = [{"msg": ErrorsDict.Get.ByCode(300)}]
 
             return json.dumps({"success": connectionStatus, "errors": Errors, "data": connectionData})
+
+    def Insert(self):
+
+        connection          = json.loads(ConnectDataBase.Status(self))
+        connectionStatus    = list(connection.values())[0]
+        connectionErrors    = list(connection.values())[1]
+        connectionData      = list(connection.values())[2]
+
+        if connectionStatus:        
+
+            Success      = True
+            Errors      = []
+            Data        = []
+            FormData    = request.forms
+
+            try:
+
+                MandatoryVars = ["cpf", "nome", "nascimento", "idenfermidade", "estagio_enfermidade"]
+
+                MandatoryVarsExists = True
+
+                for var in MandatoryVars:
+
+                    if var not in FormData.keys():
+
+                        MandatoryVarsExists = False
+
+                MandatoryVarsTypes = True
+
+                if MandatoryVarsExists:
+
+                    cpf      = FormData.get("cpf")
+                    nome     = FormData.get("nome")
+                    nascimento  = FormData.get("nascimento")
+                    idenfermidade = FormData.get("enfermidade")
+                    estagio_enfermidade = FormData.get("estagio_enfermidade")
+                    
+                    MandatoryVarsTypes = True   if  (       str(idenfermidade).isnumeric() \
+                                                            and StringHandling.isdatetime(nascimento) \
+                                                    ) \
+                                                else False
+
+                else:
+
+                    MandatoryVarsTypes = False
+
+                if MandatoryVarsExists and MandatoryVarsTypes:
+
+                    try:
+                        
+                        cpf                 = FormData.get("cpf")
+                        nome                = FormData.get("nome")
+                        nascimento          = FormData.get("nascimento")
+                        idenfermidade       = FormData.get("enfermidade") if "enfermidade" in FormData.keys() else None
+                        idcidade            = FormData.get("cidade") if "cidade" in FormData.keys() else None
+                        idestado            = FormData.get("estado") if "estado" in FormData.keys() else None 
+                        bairro              = FormData.get("bairro")
+                        endereco            = FormData.get("endereco")
+                        complemento         = FormData.get("complemento")
+                        bit_alerta          = FormData.get("bit_alerta")
+                        ind_sit             = FormData.get("ind_sit")
+
+                        cpf                 = StringHandling.CleanSqlString(cpf)
+                        nome                = StringHandling.CleanSqlString(nome)
+                        estagio_enfermidade = StringHandling.CleanSqlString(estagio_enfermidade)
+                        bairro              = StringHandling.CleanSqlString(bairro)
+                        endereco            = StringHandling.CleanSqlString(endereco)
+                        complemento         = StringHandling.CleanSqlString(complemento)
+                            
+                        SQL = " INSERT INTO PACIENTE (" + \
+                                                            "CPF , " + \
+                                                            "NOME , " + \
+                                                            "NASCIMENTO , " + \
+                                                            "IDENFERMIDADE , " + \
+                                                            "ESTAGIO_ENFERMIDADE , " + \
+                                                            "IDESTADO , " + \
+                                                            "IDCIDADE , " + \
+                                                            "BAIRRO , " + \
+                                                            "ENDERECO , " + \
+                                                            "COMPLEMENTO , " + \
+                                                            "BIT_ALERTA , " + \
+                                                            "IND_SIT" + \
+                                                        ")" + \
+                                "VALUES " + " ('{}', '{}', '{}', {}, '{}', {}, {}, '{}','{}','{}', 1, 1) RETURNING *".format(
+                                    cpf ,
+                                    nome ,
+                                    nascimento ,
+                                    idenfermidade ,
+                                    estagio_enfermidade ,
+                                    idestado ,
+                                    idcidade ,
+                                    bairro ,
+                                    endereco ,
+                                    complemento 
+                                )
+
+                        try:
+
+                            cur = self.conn.cursor()
+                            cur.execute(SQL)
+                            row_headers = [x[0] for x in cur.description]
+                            rv = cur.fetchall()
+                            self.conn.commit()
+
+                            for result in rv:
+
+                                Data.append(dict(zip(row_headers, result)))
+                                break
+
+                            if not Data:
+
+                                Success = False
+                                Errors.append({"msg": ErrorsDict.errorcode(531)})
+                        
+                        except:
+                            
+                            self.conn.rollback()
+                            Success = False
+                            Errors.append({"msg": ErrorsDict.errorcode(532)})
+                        
+                        finally:
+                        
+                            cur.close()
+                    
+                    except:
+                    
+                        Success = False
+                        Errors.append({"msg": ErrorsDict.errorcode(533)})
+
+                elif MandatoryVarsExists == False:
+
+                    Success = False
+                    Errors.append({"msg": ErrorsDict.errorcode(112)})
+
+                else: #MandatoryVarsTypes == False
+
+                    Success = False
+                    Errors.append({"msg": ErrorsDict.errorcode(113)})
             
+            except:
+
+                Success = False
+                Errors.append({"msg": ErrorsDict.errorcode(114)})
+
+            finally:
+
+                return json.dumps({"success": Success, "errors": Errors, "data": Data})
+
+        else:
+
+            # connectionErrors só será passado para usuarioid 1(suporte)
+            Errors = [{"msg": ErrorsDict.errorcode(300)}]
+
+            return json.dumps({"success": connectionStatus, "errors": Errors, "data": connectionData})
+
+    #def Update(self):
+
+
+
+    def Delete(self):
+
+        connection          = json.loads(ConnectDataBase.Get.Status(self))
+        connectionStatus    = list(connection.values())[0]
+        connectionErrors    = list(connection.values())[1]
+        connectionData      = list(connection.values())[2]
+
+        if connectionStatus:
+
+            Success      = True
+            Errors      = []
+            Data        = []
+            FormData    = request.forms
+
+            try:
+
+                idpaciente = FormData.get("id") if "id" in FormData.keys() else None
+
+                if idpaciente != None and str(idpaciente).isnumeric():
+
+                    request.query.update({'idbusca': f'{idpaciente}'})
+                    DataBefore          = json.loads(route.GetById(self))
+                    DataBeforeStatus    = list(DataBefore.values())[0]
+                    DataBeforeErrors    = list(DataBefore.values())[1]
+                    DataBeforeData      = list(DataBefore.values())[2]
+                    Count               = 0
+                    IndSitPosition      = -1
+
+                    if DataBeforeData:               
+
+                        Keys = list(DataBeforeData)[0].keys()
+
+                        for i in Keys:
+
+                            if i == "ind_sit":
+
+                                IndSitPosition = Count
+
+                            Count = Count + 1
+
+                    if  DataBeforeStatus and DataBeforeData and IndSitPosition != -1:
+
+                        if  list(list(DataBeforeData)[0].values())[IndSitPosition] != 2:
+
+                            SQL = f"UPDATE PACIENTE SET IND_SIT = 2 WHERE IDPACIENTE = {idpaciente}"
+
+                            try:
+
+                                cur = self.conn.cursor()
+                                cur.execute(SQL)
+                                self.conn.commit()
+
+                                try:
+
+                                    SQL =  f"SELECT 1 FROM PACIENTE " + \
+                                           f"WHERE IDPACIENTE = {idpaciente} AND IND_SIT <> 2 " + \
+                                            "ORDER BY IDPACIENTE"
+
+                                    cur = self.conn.cursor()
+                                    cur.execute(SQL)
+                                    row_headers = [x[0] for x in cur.description]
+                                    rv = cur.fetchall()
+                                    self.conn.commit()
+
+                                    for result in rv:
+
+                                        Data.append(dict(zip(row_headers, result)))
+                                        break
+
+                                    if Data:
+
+                                        Success = False
+                                        Errors.append({"msg": ErrorsDict.Get.ByCode(671)})
+
+                                except:
+
+                                    self.conn.rollback()
+                                    Success = False
+                                    Errors.append({"msg": ErrorsDict.Get.ByCode(672)})
+
+                                finally:
+                                
+                                    cur.close()
+                            
+                            except:
+
+                                self.conn.rollback()                
+                                Success = False
+                                Errors.append({"msg": ErrorsDict.Get.ByCode(673)})
+                            
+                            finally:
+
+                                cur.close()
+                        
+                        else:
+                        
+                            Success = False
+                            Errors.append({"msg": ErrorsDict.Get.ByCode(674)})
+                    
+                    else:
+                    
+                        Success = False
+                        Errors.append({"msg": ErrorsDict.Get.ByCode(644)})
+
+                        for error in DataBeforeErrors:
+
+                            Errors.append({"msg": list(error.values())[0]})
+
+                else:
+
+                    Success = False
+                    Errors.append({"msg": ErrorsDict.Get.ByCode(107)})
+
+            except:
+
+                Success = False
+                Errors.append({"msg": ErrorsDict.Get.ByCode(675)})
+
+            finally:
+
+                return json.dumps({"success": Success, "errors": Errors, "data": Data})
+
+        else:
+
+            # connectionErrors só será passado para usuarioid 1(suporte)
+            Errors = [{"msg": ErrorsDict.Get.ByCode(300)}]
+
+            return json.dumps({"success": connectionStatus, "errors": Errors, "data": connectionData})        
