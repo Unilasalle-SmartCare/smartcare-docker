1bed86c567ce4b5eb2fcf5b1539b7e987f0dc59e
Matthew Araujo <90223014+MatthewAraujo@users.noreply.github.com>
2023-06-02 18:02:04 -0300

    Update web-api.sh

--- smartcare-compose/apis/web-api/web-api.sh
+++ smartcare-compose/apis/web-api/web-api.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-pip install psycopg2==2.7.3.2 bottle==0.12.13 requests pypika==0.48.9
+pip install psycopg2==2.7.3.2 bottle==0.12.13 requests pypika==0.48.9 bottle-cors-plugin
 python -u web-api.py
